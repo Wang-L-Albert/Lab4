517c8f4ffd455b5c2cfa207f072ce9f419a5bb57
Albert Wang <wang.l.albert@gmail.com>
2016-03-09 00:06:20 -0800

    bugfix1

--- addtest.c
+++ addtest.c
@@ -1,11 +1,10 @@
+#define _GNU_SOURCE
 #include <unistd.h>
 #include <stdio.h>
-
 #include <string.h>
 #include <getopt.h>
 #include <stdlib.h>
 #include <time.h>
-
 
 #define noarg 0
 #define hasarg 1
@@ -23,13 +22,14 @@ void add(long long *pointer, long long value) {
 int main(int argc, char* argv[]){
     int exitNum;
     exitNum = 0;
-    struct timespec startTime, endTime;
+    struct timespec startTime;
+    struct timespec endTime;
 
     struct option optionList[]= {
         {"threads=", hasarg, noflag, THREADS},
         {"iterations=", hasarg, noflag, ITERATIONS},
-        {0,0,0,0},
-    }
+        {0,0,0,0}
+    };
     int numThreads;
     int numIterations;
     //initialize default values
